f9addce81ba236f610f2b965df3e21ab661edc76
hectorgomeya <hectorgomeya@gmail.com>
2022-08-22 13:31:02 +0200

    prueba 2

--- cypress/integration/pruebaenter.cy.js
+++
@@ -1,9 +0,0 @@
-describe('visitar titulo', () => {
-    it('passes', () => {
-      cy.log('Prueba CYPRESS')
-      cy.visit('https://www.google.com/')
-      cy.wait(3000)
-cy.get("[name='q']").type("cypres io {enter}")
-
-    })
-  })

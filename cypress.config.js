30a8073d90b50cd37b7f9313a31685da81e8c1ce
hectorgomeya <111739998+hectorgomeya@users.noreply.github.com>
2023-01-04 12:36:19 +0100

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -1,21 +1,3 @@
 const { defineConfig } = require("cypress");
 
-//module.exports = defineConfig({
-//e2e: {
-//setupNodeEvents(on, config) {
-// implement node event listeners here
-// },
-//},
-//}
 
-//);
---disable-gpu;
-module.exports = defineConfig({
-  experimentalStudio: true,
-
-  e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
-  },
-});
